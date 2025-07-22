88347d26ee5ac3cb0005db3a6df80c299ca17795
amahle comfort <anxumalo000@gmail.com>
2025-07-22 02:56:53 +0200

    check server

---
+++ backend-app/database/connect.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+/*async function ConnectDB(){
+    try{
+        await mongoose.connect(process.env.mongoURL)
+        console.log('mongodb connected successfully')
+    }catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}*/
+
+const uri = `mongodb+srv://anxumalo000:${process.env.PASSWORD}@cluster0.mb1i07x.mongodb.net/travel?retryWrites=true&w=majority&appName=Cluster0`;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function ConnectDB() {
+
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+
+module.exports = ConnectDB
---
+++ backend-app/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config({quiet:true})
+
+const ConnectDB = require("./database/connect")
+const express = require("express")
+const OfferRouter = require("./routes/offer")
+const LocationRouter = require("./routes/locations")
+const cors = require('cors')
+
+
+ConnectDB()
+
+const app = express()
+
+app.use(cors())
+
+app.use('/offers',OfferRouter)
+app.use("/locations",LocationRouter)
+
+app.listen(8888,()=>console.log('started listening on port 8888....'))
